ea336aa9bf458573f1257bf9d4d6bef637353c36
Colin McGrath <cmcgrath454@gmail.com>
2023-03-27 15:27:35 -0500

    changed placeholder text

--- src/markdown-app/MarkdownApp.js
+++ src/markdown-app/MarkdownApp.js
@@ -6,16 +6,33 @@ import { useEffect, useState } from 'react';
 
 const exampleText = `# This is a heading element
 ## This is a subheading element
+Here's some inline code, \`<div></div>\`
+\`\`\`
+// Here's a code block
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+You can make lists
 - List Item 1
-- List Item 2`
+- List Item 2
+
+**Bold text**
+
+You can add [links](https://google.com)
+
+> or Block Quotes!
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
 
 function MarkdownApp() {
     const [input, setInput] = useState(exampleText);
     const [markdown, setMarkdown] = useState('');
 
-
     useEffect(() => {
-        setMarkdown(parse(input));
+        setMarkdown(parse(input), [{breaks:true}]);
     }, [input]);
 
     return (
@@ -23,6 +40,7 @@ function MarkdownApp() {
             <Navbar header='Markdown Editor' />
             <MarkdownEditor text={input} setText={setInput} />
             <MarkdownPreview markdown={markdown} />
+            <script src="test.js"></script>
         </>
     )
 }
