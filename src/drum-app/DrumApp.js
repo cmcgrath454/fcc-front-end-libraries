bb6f601504686025f04d8adfb73c8029e6cd01ad
Colin McGrath <cmcgrath454@gmail.com>
2023-03-30 09:19:20 -0500

    styled drums and added ids for certification tests

--- src/drum-app/DrumApp.js
+++ src/drum-app/DrumApp.js
@@ -1,22 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import DrumButton from "./DrumButton";
 
 const sounds = [
-    { key: 'Q', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
-    { key: 'W', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
-    { key: 'E', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
-    { key: 'A', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
-    { key: 'S', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
-    { key: 'D', source: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
-    { key: 'Z', source: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
-    { key: 'X', source: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
-    { key: 'C', source: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
+    { key: 'Q', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', name: 'Heater 1' },
+    { key: 'W', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', name: 'Heater 2' },
+    { key: 'E', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', name: 'Heater 3' },
+    { key: 'A', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', name: 'Heater 4' },
+    { key: 'S', source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', name: 'Clap' },
+    { key: 'D', source: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', name: 'Open HH' },
+    { key: 'Z', source: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', name: 'Kick & Hat' },
+    { key: 'X', source: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', name: 'Kick' },
+    { key: 'C', source: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', name: 'Closed HH' },
 ];
 
 function DrumApp() {
+    const [soundPlayed, setSoundPlayed] = useState('');
+
     function getAndPlaySound(event) {
-        document.getElementById(event.key.toUpperCase() + '-sound-btn')?.click();
+        const btn = document.getElementById(event.key.toUpperCase() + '-sound-btn');
+        btn?.focus();
+        btn?.click();
+        btn?.blur();
+
     }
 
     useEffect(() => {
@@ -29,9 +35,15 @@ function DrumApp() {
 
     return (
         <>
-            <Navbar header="Drum Kit" />
-            <div>
-                {sounds.map(sound => <DrumButton key={sound.key} keyLetter={sound.key} audioSource={sound.source} />)}
+            <Navbar header='Drum Kit' />
+            <div id='drum-machine'>
+                <div id="display">{soundPlayed}</div>
+                <div id="drum-pad">
+                    {sounds.map(sound => <DrumButton
+                        key={sound.key}
+                        sound={sound}
+                        setSoundPlayed={setSoundPlayed} />)}
+                </div>
             </div>
         </>
 
--- src/drum-app/DrumButton.js
+++ src/drum-app/DrumButton.js
@@ -1,11 +1,19 @@
 function DrumButton(props) {
     function playSound(event) {
-        new Audio(props.audioSource).play();
+        event.preventDefault();
+        event.target.classList.add('clicked');
+        setTimeout(() => {
+            event.target.classList.remove('clicked');
+        }, 500)
+        event.target.children[0].currentTime = 0;
+        event.target.children[0].play();
+        props.setSoundPlayed(props.sound.name);
     }
 
     return (
-        <button onClick={(event) => playSound(event)} id={props.keyLetter + '-sound-btn'}>
-            {props.keyLetter}
+        <button onClick={(event) => playSound(event)} id={props.sound.key + '-sound-btn'} className='drum-pad'>
+            <audio src={props.sound.source} id={props.sound.key} className='clip'></audio>
+            <span className="text">{props.sound.key}</span>
         </button>
     );
 }
